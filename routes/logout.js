9d0b25ee63e2c44464d1c757773f27c0334b2094
a-shubh <shubham.saini@pingsafe.com>
2023-02-06 11:13:09 +0530

    npm_config_message

--- routes/logout.js
+++ routes/logout.js
@@ -4,16 +4,19 @@ const router = require('express').Router()
 
 router.get('/', verify, async(req, res) => {
     try {
+        console.log('Trying to logout');
         res.clearCookie("secret")
+        console.log('Token deleted');
         const {queueName} = req.body
         await MQServices.publishToQueue(queueName, "Logout");
+        console.log('Loged out');
         res.status(200).send({
             'msg': 'Logout Successful',
             'message-sent' : true
         })
     } catch (err) {
         console.log(err.toString());
-        res.status(500).send('Unable to logout')
+        res.status(500).send(err.toString())
     }
 })
 
